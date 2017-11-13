272c109468bcfc3ae8ad4ac014866e1841fe340c
Marcela Vilches <m.vilchespino@gmail.com>
2017-11-12 22:06:02 -0300

    Subiendo version de trello

--- assets/js/trello.js
+++ assets/js/trello.js
@@ -2,6 +2,7 @@ window.onload = function(){
 	
 	var sendButton = document.getElementById("boton");//se rescata con posiciones por que sta con clases no con id
     var thinkInput = document.getElementById("lista");
+    var thinkInput1 = document.getElementById("lista2");
     var timeLine = document.getElementById("timeline");
 
 	
@@ -11,8 +12,25 @@ window.onload = function(){
 
 		timeline.appendChild(cuadrolista);
 		cuadrolista.appendChild(textocuadro);
+		timeline.className = "tarjeta"
 
 		textocuadro.textContent = thinkInput.value;
+		thinkInput.value = "";
+		thinkInput.className = "textotrello"
+
+	}
+
+		sendButton.onclick = function(){
+		var cuadrolista2 = document.createElement("div");
+		var textocuadro = document.createElement("p");
+
+		timeline.appendChild(cuadrolista2);
+		cuadrolista2.appendChild(textocuadro);
+		timeline.className = "tarjeta"
+
+		textocuadro.textContent = thinkInput1.value;
+		thinkInput1.value = "";
+		thinkInput1.className = "textotrello"
 
 	}
 
